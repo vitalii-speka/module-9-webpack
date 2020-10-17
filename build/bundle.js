2fba3be3899647f1c7b03e82796e7edb67422565
Vitalii Speka <spekava@gmail.com>
2020-10-17 20:27:33 +0300

    +

--- build/bundle.js
+++ build/bundle.js
@@ -1,1 +1,17 @@
-console.log("test");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\Professional\\\\Documents\\\\GitHub\\\\module-9-webpack\\\\babel.config.json: Error while parsing config - JSON5: invalid end of input at 1:1\\n    at syntaxError (C:\\\\Users\\\\Professional\\\\Documents\\\\GitHub\\\\module-9-webpack\\\\node_modules\\\\json5\\\\lib\\\\parse.js:1083:17)\\n    at invalidEOF (C:\\\\Users\\\\Professional\\\\Documents\\\\GitHub\\\\module-9-webpack\\\\node_modules\\\\json5\\\\lib\\\\parse.js:1032:12)\\n    at Object.start (C:\\\\Users\\\\Professional\\\\Documents\\\\GitHub\\\\module-9-webpack\\\\node_modules\\\\json5\\\\lib\\\\parse.js:821:19)\\n    at Object.parse (C:\\\\Users\\\\Professional\\\\Documents\\\\GitHub\\\\module-9-webpack\\\\node_modules\\\\json5\\\\lib\\\\parse.js:32:32)\\n    at C:\\\\Users\\\\Professional\\\\Documents\\\\GitHub\\\\module-9-webpack\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\configuration.js:242:31\\n    at C:\\\\Users\\\\Professional\\\\Documents\\\\GitHub\\\\module-9-webpack\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\utils.js:36:12\\n    at Generator.next (<anonymous>)\\n    at Function.<anonymous> (C:\\\\Users\\\\Professional\\\\Documents\\\\GitHub\\\\module-9-webpack\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\gensync-utils\\\\async.js:26:3)\\n    at Generator.next (<anonymous>)\\n    at evaluateSync (C:\\\\Users\\\\Professional\\\\Documents\\\\GitHub\\\\module-9-webpack\\\\node_modules\\\\gensync\\\\index.js:244:28)\");\n\n//# sourceURL=webpack://module-9-webpack/./src/index.js?");
+/******/ })()
+;
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    module: {
+  entry: './src/index.js',
+  module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+        test: /\.scss$/i,
+        use: ['style-loader', 'css-loader','sass-loader',],
+        },
+        {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ],
   },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+    },
+    plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new MiniCssExtractPlugin({ filename: 'styles.css' }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    port: 4444,
+    open: true,
+    stats: 'errors-only',
   },
+
 };
